e372cf3ad50df3e560ed198f7e398bbd1207a2d3
stevenjma <sjma@NN123-HA-12.HS.sd203.org>
2015-08-27 14:12:57 -0500

    Began Chapter 2 Practice

---
+++ Chapter 2 Practice/String1.java
@@ -0,0 +1,11 @@
+public class String1
+{
+    public static void main( String[] args)
+    {
+        String greeting = "Hello, World!";
+        int n = greeting.length();
+        
+        System.out.println("Length = " + n);
+        
+    }
+}
